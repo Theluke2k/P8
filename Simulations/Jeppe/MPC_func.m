549efcf4740e8be78b8ae44bb1b473fce1c6eead
Jeppe Høvring Broe <jbroe21@student.aau.dk>
2025-04-29 12:15:24 +0200

    simulations

---
+++ Simulations/Jeppe/MPC_func.m
@@ -0,0 +1,232 @@
+function [Z_sol,U_sol,P_trace] = MPC_func(rob_init, kf_init, mpc_params, cost_params, n_robots, map_bounds, min_dist, sim_params, verbose_opt)
+%% Parameters
+import casadi.*
+
+% Map boundaries
+xmin = map_bounds(1); xmax = map_bounds(2);
+ymin = map_bounds(3); ymax = map_bounds(4);
+dmin = min_dist;
+
+% MPC parameters
+Hp = mpc_params(1); % Prediction horizon
+Hu = mpc_params(2); % Control horizon
+
+% MPC cost function parameters
+lambda1 = cost_params(1); % Weight for smooth control
+lambda2 = cost_params(2); % Weight for uncertainty
+eps = cost_params(3); % Regularization parameter
+
+% Simulation parameters
+Ts = sim_params(1); % MPC sampling time
+dt = sim_params(2); % Simulation sampling time
+
+% MISC
+N = n_robots; % Number of robots
+
+%% KALMAN FILTER
+
+% KF Dynamic model
+tau_beta = 0.998;       %spread increase parameter (exponential decay of beta)
+
+% Inputs
+beta0 = 0; %steady-state value for beta
+u_beta = beta0*(1-tau_beta)/dt; % constant input to achieve beta0 in steady-state
+u_M = 2; % Leakage rate offset
+u_KF = ones(2,Hp);
+u_KF(1,:) = u_KF(1,:)*u_M;
+u_KF(2,:) = u_KF(2,:)*u_beta;
+
+A_KF = [1 dt 0 0 0 0 0 0;
+     0 1 0 0 0 0 0 0;
+     0 0 tau_beta dt 0 0 0 0;
+     0 0 0 1 0 0 0 0;
+     0 0 0 0 1 dt 0 0;
+     0 0 0 0 0 1 0 0;
+     0 0 0 0 0 0 1 dt;
+     0 0 0 0 0 0 0 1];
+
+B_KF = zeros(8,2);
+B_KF(1,1) = dt; 
+B_KF(3,2) = dt;
+
+% Process noise
+G = [0.5*dt^2 0 0 0;
+    dt 0 0 0;
+    0 0.5*dt^2 0 0;
+    0 dt 0 0;
+    0 0 0.5*dt^2 0;
+    0 0 dt 0;
+    0 0 0 0.5*dt^2;
+    0 0 0 dt];
+sigma_M = 0.2; sigma_beta = 0.0001; sigma_x = 0.01; sigma_y = 0.01;
+sigma = [sigma_M sigma_beta sigma_x sigma_y]';
+mu_w = zeros(length(sigma),1);
+Q = G*diag([sigma_M sigma_beta sigma_x sigma_y])*G';
+
+% Measurement model - h(z,x,y) = (M*beta/pi)*exp(-beta*((x-xs).^2 + (y-ys).^2))
+h = @(states,x,y) (states(1)*states(3)/pi)*exp(-states(3)*((x-states(5)).^2 + (y-states(7)).^2));
+
+% Measurement noise
+sigma_measurement = 0.1;
+R_KF = sigma_measurement*eye(N); % measurement noise covariance
+
+%% Robot model
+
+% Robot model (integrator)
+A = eye(2); % z = [x;y]
+B = Ts*eye(2); % u = [vx;vy]
+C = eye(2);
+D = 0;
+n = size(A,1);
+l = size(B,2);
+m = size(C,1);
+
+% Reformulation to Delta u
+A_a = [A,B; zeros(n,n),eye(n)]; % X(k) = [x(k),y(k),vx(k-1),vy(k-1)]^T
+B_a = [B; eye(l)]; % Du(k) = [vx(k)-vx(k-1), vy(k)-vy(k-1)]^T
+C_a = [C, zeros(n,m)]; % za(k) = [x(k),y(k),vx(k-1),vy(k-1)]^T
+D_a = 0; % DU(k) = [vx(k)-vx(k-1), vy(k)-vy(k-1)]^T
+n_a = size(A_a,1);
+l_a = size(B_a,2);
+m_a = size(C_a,1);
+
+% Expanded to include all N robots (DEFINED INCORRECTLY -> Assumes [x1;y1;vx1;vy1;x2;y2;...] should assume [x1;y1;x2;y2;...;vx1;vy1;...])
+A_aN = kron(eye(N), A_a);
+B_aN = kron(eye(N), B_a);
+C_aN = kron(eye(N), C_a);
+D_aN = 0;
+
+%% MPC object
+% Setup opti
+opti = Opti();
+
+% Decision variables (to be optimized over)
+DU = opti.variable(N*l_a,Hu);
+Za = opti.variable(N*n_a,Hp+1);
+
+P_KF_est = opti.variable(8*Hp,8);
+P_KF_pred = opti.variable(8*(Hp+1),8);
+
+Z_KF = opti.variable(8,Hp+1);
+h_KF = opti.variable(N,Hp);
+
+H_KF = opti.variable(N,8);
+K_KF = opti.variable(8,N);
+
+% Parameters (NOT to be optimized over)
+Z0 = opti.parameter(N*n,1);
+Uprev = opti.parameter(N*l,1);
+Z0_KF = opti.parameter(8,1);
+P0_KF = opti.parameter(8,8);
+
+% Cost function
+cost = 0;
+for k = 1:Hu
+    cost = cost + DU(:,k)'*DU(:,k); % Assuming R = I
+end
+
+% KF algorithm
+cost_KF = 0;
+Z_KF(:,1) = Z0_KF;
+P_KF_pred(1:8,:) = P0_KF; % P(k|k-1)
+for k = 1:Hp
+    for j = 1:N
+        % Positions
+        x_pos = Za(1+(j-1)*n_a,k);
+        y_pos = Za(2+(j-1)*n_a,k);
+
+        % Measurement predictions
+        h_KF(j,k) = h(Z_KF(:,k), x_pos, y_pos);
+
+        % Linearize
+        H_KF(j,1) = h_KF(j,k)/(Z_KF(1,k)+eps);
+        H_KF(j,3) = -h_KF(j,k)*((x_pos-Z_KF(5,k))^2 + (y_pos-Z_KF(7,k))^2) + h_KF(j,k)/(Z_KF(3,k)+eps);
+        H_KF(j,5) = -2*h_KF(j,k)*Z_KF(3,k)*(Z_KF(5,k)-x_pos);
+        H_KF(j,7) = -2*h_KF(j,k)*Z_KF(3,k)*(Z_KF(7,k)-y_pos);
+    end
+    K_KF = P_KF_pred(8*k-7:8*k,:)*H_KF'*inv(H_KF*P_KF_pred(8*k-7:8*k,:)*H_KF' + R_KF);
+
+    P_KF_est(8*k-7:8*k,:) = (eye(8) - K_KF*H_KF)*P_KF_pred(8*k-7:8*k,:)*(eye(8) - K_KF*H_KF)' + K_KF*R_KF*K_KF'; % P(k|k)
+
+    % Time update
+    Z_KF(:,k+1) = A_KF*Z_KF(:,k) + B_KF*u_KF(:,k);
+    P_KF_pred(8*(k+1)-7:8*(k+1),:) = A_KF*P_KF_est(8*k-7:8*k,:)*A_KF' + Q;
+
+    % Add to cost
+    cost_KF = cost_KF + log(abs(trace(P_KF_pred(8*(k+1)-7:8*(k+1),:) + eps*eye(8))));
+end
+
+% Constraints
+Za0 = [reshape(Z0,n,N); reshape(Uprev,l,N)];
+opti.subject_to(Za(:,1) == reshape(Za0,N*(n+l),1));
+for k = 1:Hp
+    % Dynamics
+    if k <= Hu
+        opti.subject_to(Za(:,k+1) == A_aN*Za(:,k) + B_aN*DU(:,k));
+    else
+        opti.subject_to(Za(:,k+1) == A_aN*Za(:,k)); % DU(k > Hu) = 0
+    end
+
+    % Box constraints
+    opti.subject_to(xmin <= Za(1:n_a:N*n_a,k+1) <= xmax);
+    opti.subject_to(ymin <= Za(2:n_a:N*n_a,k+1) <= ymax);
+    
+    % Collision avoidance
+    for i = 1:N
+        for j = i+1:N
+            xi = Za((i-1)*n_a + 1, k+1);
+            yi = Za((i-1)*n_a + 2, k+1);
+            xj = Za((j-1)*n_a + 1, k+1);
+            yj = Za((j-1)*n_a + 2, k+1);
+
+            dist_squared = (xi - xj)^2 + (yi - yj)^2;
+            opti.subject_to(dist_squared >= dmin^2);
+        end
+    end
+end
+
+% Outputs
+Z_opt = [];
+U_opt = [];
+for i = 1:n_a:N*n_a
+    Z_opt = [Z_opt; Za(i,:); Za(i+1,:)];
+    U_opt = [U_opt; Za(i+2,:); Za(i+3,:)];
+end
+
+% Define MPC 'object'
+opti.minimize(lambda1*cost + lambda2*cost_KF);
+
+solver_opts = struct;
+solver_opts.ipopt.max_iter = 1000;
+solver_opts.ipopt.tol = 1e-6;
+solver_opts.ipopt.print_level = verbose_opt;
+% solver_opts.print_time = false;
+opti.solver('ipopt', solver_opts);
+
+%% Call MPC
+% Initialization
+Z0_val = rob_init(:,1); % Initial robot states
+Uprev_val = rob_init(:,2); % Previous control input
+Z0_KF_val = kf_init(:,1); % Initial prediction for kalman filter
+P0_KF_val = kf_init(:,2:end); % Initial error covariance
+
+opti.set_value(Z0, Z0_val);
+opti.set_value(Uprev, Uprev_val);
+opti.set_value(Z0_KF, Z0_KF_val);
+opti.set_value(P0_KF, P0_KF_val);
+
+% Solve
+sol = opti.solve();
+
+% Outputs
+Z_sol = sol.value(Z_opt);
+U_sol = sol.value(U_opt);
+
+P_sol = sol.value(P_KF_pred);
+P_trace = [];
+for k = 1:Hp+1
+    P_trace = [P_trace, log(trace(P_sol(8*k-7:8*k,:)))];
+end
+
+end
+
---
+++ Simulations/Jeppe/RunningExample_MPC.m
@@ -0,0 +1,265 @@
+%Radiation/chemical leak (running example) model parameter estimation
+clc; clear; close all;
+
+%% Process simulation parameters
+N = 3;                  % Number of robots
+dt = 0.1;               % Time step [s]
+sim_time = 10;          % Simulation time [s]
+T = sim_time/dt;        % Total # of simulation steps
+Ts = 1;                 % MPC sampling time
+sim_params = [Ts, dt];
+
+% MPC and cost params
+Hp = 6; Hu = 3;
+mpc_params = [Hp, Hu];           % Hp, Hu
+cost_params = [1, 1e4, 1e-6];    % lambda1, lambda2, epsilon
+min_dist = 0.5;                  % Minimum distance between robots
+
+% Map bounderies
+xmin = 0; xmax = 20;
+ymin = 0; ymax = 20;
+map_bounds = [xmin, xmax, ymin, ymax];
+
+% True initial model parameters
+M_ref = 200;
+beta_ref = 0.1;
+xs_ref = 10;
+ys_ref = 10;
+
+% Initial derivative states
+M_dot = 0;
+beta_dot = 0;
+xs_dot = 0;
+ys_dot = 0;
+
+% Initial true state vector
+z = zeros(8,T+1);
+z(:,1) = [M_ref; M_dot; beta_ref; beta_dot; xs_ref; xs_dot; ys_ref; ys_dot]; %initial state vector
+
+% Initial model parameters guess
+M = 1e-3;
+beta = 1e-3;
+xs = 8;
+ys = 12;
+
+% Initial states and error covariance matrix
+z_pred = [M; M_dot; beta; beta_dot; xs; xs_dot; ys; ys_dot]; % Initial prediction for kalman filter
+
+% Initial error covariance
+P_pred = 1e-6*ones(size(z,1));
+for i = 1:2:3
+    P_pred(i,i) = 1e-1;
+end
+kf_init = [z_pred, P_pred];
+
+% Initial robot positions, Z0 and control inputs, Uprev
+X0 = zeros(2*N,1); % (assuming N=4, so 2*N values)
+for j = 1:N
+    X0(2*j-1) = j;
+end
+Uprev = zeros(2*N,1);           % (2*N values again)
+
+
+%% Dynamic parameter model
+tau_beta = 0.998;       %spread increase parameter (exponential decay of beta)
+
+% Inputs
+beta0 = 0; %steady-state value for beta
+u_beta = beta0*(1-tau_beta)/dt; % constant input to achieve beta0 in steady-state
+u_M = 2; % Leakage rate offset
+u = ones(2,T);
+u(1,:) = u(1,:)*u_M;
+u(2,:) = u(2,:)*u_beta;
+
+A = [1 dt 0 0 0 0 0 0;
+     0 1 0 0 0 0 0 0;
+     0 0 tau_beta dt 0 0 0 0;
+     0 0 0 1 0 0 0 0;
+     0 0 0 0 1 dt 0 0;
+     0 0 0 0 0 1 0 0;
+     0 0 0 0 0 0 1 dt;
+     0 0 0 0 0 0 0 1];
+
+B = zeros(size(z,1),2);
+B(1,1) = dt; B(3,2) = dt;
+
+% Process noise
+G = [0.5*dt^2 0 0 0;
+    dt 0 0 0;
+    0 0.5*dt^2 0 0;
+    0 dt 0 0;
+    0 0 0.5*dt^2 0;
+    0 0 dt 0;
+    0 0 0 0.5*dt^2;
+    0 0 0 dt];
+sigma_M = 0.2; sigma_beta = 0.0001; sigma_x = 0.01; sigma_y = 0.01;
+sigma = [sigma_M sigma_beta sigma_x sigma_y]';
+mu_w = zeros(length(sigma),1);
+Q = G*diag([sigma_M sigma_beta sigma_x sigma_y])*G';
+
+% Measurement model - h(z,x,y) = (M*beta/pi)*exp(-beta*((x-xs).^2 + (y-ys).^2))
+h = @(states,x,y) (states(1)*states(3)/pi)*exp(-states(3)*((x-states(5)).^2 + (y-states(7)).^2));
+
+% Measurement noise
+sigma_measurement = 0.1;
+R = sigma_measurement*eye(N); % measurement noise covariance
+
+%% Predifined robot trajectories
+% pos_store = zeros(T, 2, N);
+% 
+% % Robot 1 - btm left to top right
+% pos_store(:,:,1) = [linspace(xmin,xmax,T)', linspace(ymin,ymax,T)'];
+% 
+% % Robot 2 - top left to btm right
+% pos_store(:,:,2) = [linspace(xmin,xmax,T)', linspace(ymax,ymin,T)'];
+% 
+% % Robot 3 - Circle around the center
+% theta = linspace(0,2*pi,T)';
+% r = 1.5;
+% center = [10,10];
+% pos_store(:,:,3) = [center(1) + r*cos(theta), center(2) + r*sin(theta)];
+
+%% Storage and plotting for measurements and kalman filter
+measurements = zeros(N,T);
+estimates = zeros(size(z,1),T);
+y_pred = zeros(N,1); % Storage for output prediction
+
+pos_store = zeros(T,2,N);
+
+%% Simulation loop
+x_opt = X0;
+u_opt = Uprev;
+% rob_init = [x_opt, u_opt];
+
+H = zeros(N,size(z,1)); % Initialize linearized H matrix
+for n=1:T
+    % Simulate true process
+    w = G*normrnd(mu_w,sigma);
+    z(:,n+1) = A*z(:,n) + B*u(:,n) + w;
+
+    % Measurement update
+    for i = 1:N
+        % Update robot positions
+        x_pos = x_opt(2*i-1);
+        y_pos = x_opt(2*i);
+        pos_store(n,:,i) = [x_pos, y_pos];
+
+        % Take measurements
+        v = normrnd(0,sigma_measurement);
+        measurements(i,n) = h(z(:,n), x_pos, y_pos) + v;
+        
+        % Make measurement predictions
+        y_pred(i) = h(z_pred, x_pos, y_pos);
+        
+        % Linearize
+        H(i,1) = y_pred(i)/z_pred(1);
+        H(i,3) = -y_pred(i)*((x_pos-z_pred(5))^2 + (y_pos-z_pred(7))^2) + y_pred(i)/z_pred(3);
+        H(i,5) = -2*y_pred(i)*z_pred(3)*(z_pred(5)-x_pos);
+        H(i,7) = -2*y_pred(i)*z_pred(3)*(z_pred(7)-y_pos);
+    end
+    y_error = measurements(:,n) - y_pred;
+    K = P_pred*H'*(H*P_pred*H' + R)^-1;
+    
+    estimates(:,n) = z_pred + K*y_error;
+    P_update = (eye(size(z,1)) - K*H)*P_pred*(eye(size(z,1)) - K*H)' + K*R*K';
+
+    % Time update
+    z_pred = A*estimates(:,n) + B*u(:,n);
+    P_pred = A*P_update*A' + Q;
+    
+    kf_init = [z_pred, P_pred];
+    rob_init = [x_opt, u_opt];
+    [X_opt, U_opt, P_trace] = MPC_func(rob_init, kf_init, mpc_params, cost_params, N, map_bounds, min_dist, sim_params, 3);
+    u_opt = U_opt(:,2);
+    x_opt = X_opt(:,2);
+end
+
+%% Plots 
+
+% Intensity distribution animation
+nx_vis = 50; ny_vis = 50;
+x_vis = linspace(xmin,xmax,nx_vis);
+y_vis = linspace(ymin,ymax,ny_vis);
+[X_vis, Y_vis] = meshgrid(x_vis, y_vis);
+
+figure;
+for i = 1:T
+    I_max_true = max(z(1,i))*max(z(3,i))/pi;
+    I_max_est = max(estimates(1,i))*max(estimates(3,i))/pi;
+    I_max = max(I_max_true, I_max_est);
+
+    I_min_true = min(z(1,i))*min(z(3,i))/pi;
+    I_min_est = min(estimates(1,i))*min(estimates(3,i))/pi;
+    I_min = min(I_min_true, I_min_est);
+    if I_min >= 0
+        I_min = 0;
+    end
+
+    q_true = (z(1,i)*z(3,i)/pi)*exp(-z(3,i)*((X_vis - z(5,i)).^2 + (Y_vis - z(7,i)).^2));
+    q_est = (estimates(1,i)*estimates(3,i)/pi)*exp(-estimates(3,i)*((X_vis - estimates(5,i)).^2 + (Y_vis - estimates(7,i)).^2));
+
+    subplot(1,2,1);
+    cla;
+    hold on;
+    imagesc(x_vis, y_vis, q_true);
+    for j = 1:N
+        pos = pos_store(i,:,j); % drone position at step i
+        plot(pos(1), pos(2), 'ko', 'MarkerFaceColor','w', 'MarkerSize',10);
+    end
+    hold off;
+    set(gca,'YDir','normal');
+    caxis([I_min I_max]); 
+    colorbar;
+    % title('True Radiation Field');
+    title(sprintf('True Radiation Field @t=%.3f',i*dt));
+    xlabel('x'); xlim([xmin, xmax]);
+    ylabel('y'); ylim([ymin, ymax]);
+
+    subplot(1,2,2);
+    cla;
+    hold on;
+    imagesc(x_vis, y_vis, q_est);
+    for j = 1:N
+        pos = pos_store(i,:,j); % drone position at step i
+        plot(pos(1), pos(2), 'ko', 'MarkerFaceColor','w', 'MarkerSize',10);
+    end
+    hold off;
+    set(gca,'YDir','normal');
+    caxis([I_min I_max]); 
+    colorbar;
+    % title('Estimated Radiation Field');
+    title(sprintf('Estimated Radiation Field @t=%.3f',i*dt));
+    xlabel('x'); xlim([xmin, xmax]);
+    ylabel('y'); ylim([ymin, ymax]);
+
+    drawnow limitrate;
+    pause(0.001);
+end
+
+% Plot state estimates
+figure;
+for i = 1:size(z,1)
+    subplot(size(z,1)/2,2,i);
+    hold on;
+    plot(0:T-1, z(i,1:T), DisplayName=sprintf("z_{%d}", i));
+    plot(0:T-1, estimates(i,:), '--', DisplayName=sprintf("z_{%d,est}", i));
+    hold off;
+    legend;
+    xlabel("Time [s]");
+    grid on;
+end
+
+figure;
+hold on;
+for j = 1:N
+    plot(pos_store(:,1,j), pos_store(:,2,j), '-o', DisplayName=sprintf("Robot %d",j));
+    plot(pos_store(1,1,j), pos_store(1,2,j), 'go', 'MarkerFaceColor', 'g', 'MarkerSize', 3, 'HandleVisibility', 'off');
+    plot(pos_store(end,1,j), pos_store(end,2,j), 'ro', 'MarkerFaceColor', 'r', 'MarkerSize', 3, 'HandleVisibility', 'off');
+end
+hold off;
+legend;
+xlabel("x");
+ylabel("y");
+xlim([xmin, xmax]);
+ylim([ymin ymax]);
+grid on;
