c3cd56bdfabec45964573f5d7599db157df71f62
Theluke2k <lselmer@gmail.com>
2025-04-29 22:11:02 +0200

    Added EKF.m script

---
+++ Simulations/Jeppe/EKF.m
@@ -0,0 +1,7 @@
+function [x,P] = EKF(x_prev,P_prev,A,B,H,)
+    %KALMANFILTER Summary of this function goes here
+    %   Detailed explanation goes here
+    outputArg1 = inputArg1;
+    outputArg2 = inputArg2;
+end
+
